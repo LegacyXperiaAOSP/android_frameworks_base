a8e4c949bd281a4b567636afe97d5b0d1d60855a
Michał Gapiński <mikegapinski@rMBP-Yosemite.local>
2015-03-28 00:13:53 +0100

    Enable home button wake by default

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4622,6 +4622,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             }
 
+           //enable wake on homebutton permanently
+            case KeyEvent.KEYCODE_HOME:
+                if (down && !interactive) {
+                    isWakeKey = true;
+                }
+                break;
             case KeyEvent.KEYCODE_ENDCALL: {
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
